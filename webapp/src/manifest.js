611032d7ab86c842063a89f64c64cb7c48237bab
Jason Frerich <jason.frerich@mattermost.com>
2020-04-21 11:57:40 -0500

    Bump version to 0.2.0 (#38)
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>

--- webapp/src/manifest.js
+++ webapp/src/manifest.js
@@ -5,7 +5,7 @@ const manifest = JSON.parse(`
     "id": "com.mattermost.plugin-todo",
     "name": "Todo Plugin",
     "description": "This plugin makes it easy to keep track of Todo issues and get daily reminders.",
-    "version": "0.1.0",
+    "version": "0.2.0",
     "min_server_version": "5.12.0",
     "server": {
         "executables": {
