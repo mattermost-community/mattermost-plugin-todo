1905ad536a12464b850c7991dde55f71fd7b4b2c
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2020-08-04 15:30:03 +0800

    Change channel header button to toggle RHS (#106)

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -27,9 +27,9 @@ export default class Plugin {
             (postID) => store.dispatch(openRootModal(postID)),
         );
 
-        const { showRHSPlugin } = registry.registerRightHandSidebarComponent(SidebarRight, 'Todo List');
+        const { toggleRHSPlugin, showRHSPlugin } = registry.registerRightHandSidebarComponent(SidebarRight, 'Todo List');
         store.dispatch(setShowRHSAction(() => store.dispatch(showRHSPlugin)));
-        registry.registerChannelHeaderButtonAction(<ChannelHeaderButton/>, () => store.dispatch(showRHSPlugin), 'Todo', 'Open your list of Todo issues.');
+        registry.registerChannelHeaderButtonAction(<ChannelHeaderButton/>, () => store.dispatch(toggleRHSPlugin), 'Todo', 'Open your list of Todo issues.');
 
         const refresh = () => {
             store.dispatch(list(false, 'my'));
