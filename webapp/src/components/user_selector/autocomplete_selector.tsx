d3a08fcc8e905adae645d2b5b00ff98f83ac7948
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2020-11-05 00:51:34 +0800

    Change component theme based on mattermost setting
    - Build color styles based on mattermost theme
    - Override option component to show same color for text

--- webapp/src/components/user_selector/autocomplete_selector.tsx
+++ webapp/src/components/user_selector/autocomplete_selector.tsx
@@ -4,6 +4,8 @@
 import React from 'react';
 
 import AsyncSelect from 'react-select/async';
+import {blendColors, changeOpacity, makeStyleFromTheme} from 'mattermost-redux/utils/theme_utils';
+
 import {Theme} from 'mattermost-redux/types/preferences';
 import {OptionsType, ValueType} from 'react-select/src/types';
 import {UserProfile} from 'mattermost-redux/types/users';
@@ -19,6 +21,29 @@ type Props = {
     onSelected?: (value: ValueType<UserProfile>) => void,
     theme: Theme,
 }
+
+// override react select theme based on mattermost theme
+const getColorStyles = makeStyleFromTheme((theme) => {
+    return {
+        primary: changeOpacity(theme.centerChannelColor, 0.32),
+        primary75: changeOpacity(theme.centerChannelColor, 0.24),
+        primary50: changeOpacity(theme.centerChannelColor, 0.16),
+        primary25: changeOpacity(theme.centerChannelColor, 0.08),
+        danger: theme.errorTextColor,
+        dangerLight: changeOpacity(theme.errorTextColor, 0.48),
+        neutral0: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0),
+        neutral5: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.08),
+        neutral10: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.16),
+        neutral20: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.24),
+        neutral30: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.32),
+        neutral40: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.40),
+        neutral50: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.56),
+        neutral60: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.64),
+        neutral70: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.72),
+        neutral80: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.8),
+        neutral90: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.88),
+    };
+});
 
 export default function AutocompleteSelector(props: Props) {
     const {
@@ -61,6 +86,8 @@ export default function AutocompleteSelector(props: Props) {
 
     const getOptionData = (option: UserProfile) => option.username;
 
+    const colors = getColorStyles(theme);
+
     return (
         <div
             data-testid='autoCompleteSelector'
@@ -78,6 +105,19 @@ export default function AutocompleteSelector(props: Props) {
                     getOptionLabel={getOptionData}
                     getOptionValue={getOptionData}
                     onChange={handleSelected}
+                    styles={{
+                        option: (provided, state) => ({
+                            ...provided,
+                            color: state.isDisabled ? colors.neutral30 : colors.neutral90,
+                        }),
+                    }}
+                    theme={(componentTheme) => ({
+                        ...componentTheme,
+                        colors: {
+                            ...componentTheme.colors,
+                            ...colors,
+                        },
+                    })}
                 />
                 {helpTextContent}
             </div>
