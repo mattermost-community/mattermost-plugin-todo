b8330bea13104a07120a48eff2de4dc358e4238e
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2020-11-05 23:24:55 +0800

    Abstract component theme generating functions
    - Use getColorStyles from utils
    - Implement useTheme custom hook
    - Retrieve component style and theme using new custom hook

--- webapp/src/components/user_selector/autocomplete_selector.tsx
+++ webapp/src/components/user_selector/autocomplete_selector.tsx
@@ -1,14 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useState, CSSProperties} from 'react';
 
 import AsyncSelect from 'react-select/async';
-import {OptionsType, ValueType} from 'react-select/src/types';
+import {OptionsType, ValueType, Theme as ComponentTheme} from 'react-select/src/types';
+import {Props as ComponentProps, StylesConfig} from 'react-select/src/styles';
+import {ThemeConfig} from 'react-select/src/theme';
 
-import {blendColors, changeOpacity, makeStyleFromTheme} from 'mattermost-redux/utils/theme_utils';
 import {Theme} from 'mattermost-redux/types/preferences';
 import {UserProfile} from 'mattermost-redux/types/users';
+
+import {getColorStyles} from '../../utils';
 
 type Props = {
     loadOptions: (inputValue: string, callback: ((options: OptionsType<UserProfile>) => void)) => Promise<unknown> | void,
@@ -22,28 +25,26 @@ type Props = {
     theme: Theme,
 }
 
-// override react select theme based on mattermost theme
-const getColorStyles = makeStyleFromTheme((theme) => {
-    return {
-        primary: changeOpacity(theme.centerChannelColor, 0.32),
-        primary75: changeOpacity(theme.centerChannelColor, 0.24),
-        primary50: changeOpacity(theme.centerChannelColor, 0.16),
-        primary25: changeOpacity(theme.centerChannelColor, 0.08),
-        danger: theme.errorTextColor,
-        dangerLight: changeOpacity(theme.errorTextColor, 0.48),
-        neutral0: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0),
-        neutral5: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.08),
-        neutral10: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.16),
-        neutral20: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.24),
-        neutral30: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.32),
-        neutral40: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.40),
-        neutral50: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.56),
-        neutral60: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.64),
-        neutral70: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.72),
-        neutral80: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.8),
-        neutral90: blendColors(theme.centerChannelBg, theme.centerChannelColor, 0.88),
+const useTheme = (mattermostTheme: Theme): [StylesConfig, ThemeConfig] => {
+    const [colors] = useState(getColorStyles(mattermostTheme));
+
+    const styles: StylesConfig = {
+        option: (provided: CSSProperties, state: ComponentProps) => ({
+            ...provided,
+            color: state.isDisabled ? colors.neutral30 : colors.neutral90,
+        }),
     };
-});
+
+    const compTheme: ThemeConfig = (componentTheme: ComponentTheme) => ({
+        ...componentTheme,
+        colors: {
+            ...componentTheme.colors,
+            ...colors,
+        },
+    });
+
+    return [styles, compTheme];
+};
 
 export default function AutocompleteSelector(props: Props) {
     const {
@@ -86,7 +87,7 @@ export default function AutocompleteSelector(props: Props) {
 
     const getOptionData = (option: UserProfile) => option.username;
 
-    const colors = getColorStyles(theme);
+    const [styles, componentTheme] = useTheme(theme);
 
     return (
         <div
@@ -105,19 +106,8 @@ export default function AutocompleteSelector(props: Props) {
                     getOptionLabel={getOptionData}
                     getOptionValue={getOptionData}
                     onChange={handleSelected}
-                    styles={{
-                        option: (provided, state) => ({
-                            ...provided,
-                            color: state.isDisabled ? colors.neutral30 : colors.neutral90,
-                        }),
-                    }}
-                    theme={(componentTheme) => ({
-                        ...componentTheme,
-                        colors: {
-                            ...componentTheme.colors,
-                            ...colors,
-                        },
-                    })}
+                    styles={styles}
+                    theme={componentTheme}
                 />
                 {helpTextContent}
             </div>
