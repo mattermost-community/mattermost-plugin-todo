861f546af6b609dcf7f649f9ec3b47f1f2a3b196
Rahul Suresh <rahul.tech.work@gmail.com>
2023-12-03 13:14:54 -0600

    did some cleanup in existing code

--- e2e/playwright/tests/todo_plugin.spec.ts
+++ e2e/playwright/tests/todo_plugin.spec.ts
@@ -6,37 +6,37 @@
 // - [*] indicates an assertion (e.g. * Check the title)
 // ***************************************************************
 
-import {expect, test} from '@e2e-support/test_fixture';
-import SlashCommandSuggestions from 'support/components/slash_commands';
-import {fillMessage, getTodoBotDMPageURL} from 'support/utils';
+import { expect, test } from "@e2e-support/test_fixture";
+import SlashCommandSuggestions from "support/components/slash_commands";
+import { fillMessage, getTodoBotDMPageURL } from "support/utils";
 
 export default {
-    connected: () => {
-        test.describe('available commands', () => {
-            test('with just the main command', async ({pages, page, pw}) => {
-               
-            const {adminClient, adminUser} = await pw.getAdminClient();
-            if (adminUser === null) {
-                throw new Error('can not get adminUser');
-            }
-            const dmURL = await getTodoBotDMPageURL(adminClient, '', adminUser.id);
-            await page.goto(dmURL, {waitUntil: 'load'});
+  setup: () => {
+    test("checking available commands", async ({ pages, page, pw }) => {
+      const { adminClient, adminUser } = await pw.getAdminClient();
+      if (adminUser === null) {
+        throw new Error("can not get adminUser");
+      }
+      const dmURL = await getTodoBotDMPageURL(adminClient, "", adminUser.id);
+      await page.goto(dmURL, { waitUntil: "load" });
 
-            const c = new pages.ChannelsPage(page);
-            const slash = new SlashCommandSuggestions(page.locator('#suggestionList'));
+      const c = new pages.ChannelsPage(page);
+      const slash = new SlashCommandSuggestions(
+        page.locator("#suggestionList")
+      );
 
-            // # Run incomplete command to trigger help
-            await fillMessage('/todo', page);
+      // # Run incomplete command to trigger help
+      await fillMessage("/todo", page);
 
-            // * Assert suggestions are visible
-            await expect(slash.container).toBeVisible();
+      // * Assert suggestions are visible
+      await expect(slash.container).toBeVisible();
 
-            // * Assert help is visible
-            await expect(slash.getItemTitleNth(0)).toHaveText('todo [command]');
+      // * Assert help is visible
+      await expect(slash.getItemTitleNth(0)).toHaveText("todo [command]");
 
-            await expect(slash.getItemDescNth(0)).toHaveText('Available commands: list, add, pop, send, settings, help');
-            });
-        });
-    },
+      await expect(slash.getItemDescNth(0)).toHaveText(
+        "Available commands: list, add, pop, send, settings, help"
+      );
+    });
+  },
 };
-
